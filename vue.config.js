6e80d09b4e5bcf5cd3e06e6afc682afcda9555a5
hting <26hting@gmail.com>
2022-03-01 13:34:42 +0800

    vue.config&.github

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,12 @@ module.exports = {
         additionalData: [...bootstrapSassAbstractsImports, ''].join(';\n')
       }
     }
+  },
+  publicPath: './',
+  chainWebpack (config) {
+    config.plugin('html').tap(args => {
+      args[0].title = '購物網'
+      return args
+    })
   }
 }
