831ab69fa1b9c10ac457c1930fd47c3ee43439af
hting <26hting@gmail.com>
2022-03-02 11:15:00 +0800

    volunteer.解決b-card-img無法用src的問題

--- vue.config.js
+++ vue.config.js
@@ -16,5 +16,23 @@ module.exports = {
       args[0].title = '購物網'
       return args
     })
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.transformAssetUrls = {
+          img: 'src',
+          image: 'xlink:href',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'src',
+          'b-card-img-lazy': ['src', 'blank-src'],
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src'
+        }
+        return options
+      })
   }
 }
